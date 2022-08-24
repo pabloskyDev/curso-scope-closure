287ab0973cdb37f2fe7347771766f7d4713f9d43
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2022-08-23 19:13:39 -0500

    Lexico closure

---
+++ src/closure/lexical.js
@@ -0,0 +1,23 @@
+// Closure
+// La accesibilidad de las variables está determinadas por la pocisión de las mismas en el ámbito léxico
+const myGlobal = 0;
+
+function myFunction() {
+    const myNumber = 1;
+    console.log(myGlobal);
+
+    function parent() { // funcion interna
+        const inner = 2;
+        console.log(myNumber, myGlobal);
+
+        function child() {
+            console.log(inner, myNumber, myGlobal);
+        }
+
+        return child();
+    }
+
+    return parent();
+}
+
+myFunction();
